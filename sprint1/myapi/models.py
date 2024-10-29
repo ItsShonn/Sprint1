6cfa562bc9c03748e401dbf419252243cf1fa427
ItsShonn <leonardochoned@gmail.com>
2024-10-29 14:28:10 +0300

    ChangedPerevalImages

--- sprint1/myapi/models.py
+++ sprint1/myapi/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 
+class PerevalAdded(models.Model):
+    pass
+
+
+class Images(models.Model):
+    img = models.BinaryField(Null=False)
+
+
 class PerevalImages(models.Model):
     date_added = models.DateTimeField(auto_now=True)
-    img = models.BinaryField(Null=False)
+    pereval_id = models.ForeignKey(PerevalAdded, on_delete=models.CASCADE)
+    image_id = models.ForeignKey(Images, on_delete=models.CASCADE)
+
+
