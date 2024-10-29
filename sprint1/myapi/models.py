1d114578afe66425666d72e6f71b8c9c2cbc9836
ItsShonn <leonardochoned@gmail.com>
2024-10-29 14:22:09 +0300

    PerevalImagesModel

--- sprint1/myapi/models.py
+++ sprint1/myapi/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class PerevalImages(models.Model):
+    date_added = models.DateTimeField(auto_now=True)
+    img = models.BinaryField(Null=False)
