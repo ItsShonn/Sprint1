1f6a1679d77428c1f8fa94c13827ef2d36dd4e0f
ItsShonn <leonardochoned@gmail.com>
2024-10-29 14:44:02 +0300

    MadePerevalAdded&Coords

--- sprint1/myapi/models.py
+++ sprint1/myapi/models.py
@@ -1,8 +1,27 @@
 from django.db import models
 
 
+class Coords(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    height = models.IntegerField()
+
+
 class PerevalAdded(models.Model):
-    pass
+    time_added = models.DateTimeField(auto_now=True) # OF PEREVAL ADDED OBJECT
+
+    beautyTitle = models.CharField()
+    title = models.CharField()
+    otherTitles = models.CharField()
+    connect = models.CharField()
+    add_time = models.CharField() # IN JSON DATA
+
+    spring = models.CharField(null=True)
+    summer = models.CharField(null=True)
+    autumn = models.CharField(null=True)
+    winter = models.CharField(null=True)
+
+    coords_id = models.ForeignKey(Coords, on_delete=models.CASCADE())
 
 
 class Images(models.Model):
