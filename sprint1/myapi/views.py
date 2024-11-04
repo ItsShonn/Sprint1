8148ca33958ae3cade7482077cfe2d044abe1270
ItsShonn <leonardochoned@gmail.com>
2024-11-04 23:25:45 +0300

    PATCH method done (alike Post)

--- sprint1/myapi/views.py
+++ sprint1/myapi/views.py
@@ -89,3 +89,75 @@ class submitDataGET(views.APIView):
             })
         except Exception as e:
             return Response({"status":500, "message":f"Ошибка {e}", "id":0})
+
+    def patch(self, request, format=None, **kwargs):
+        try:
+            objects_ = []
+            pk = kwargs.get('pk')
+            pereval = PerevalAdded.objects.filter(pk=pk)
+            if not(pereval.exists()) == True:
+                return Response({"state":0, "message":"Объекта нет в базе данных"})
+            pereval = pereval.first()
+
+            if pereval.status == 'n':
+
+                data = request.data
+                serializer = PerevalSerializer(data=data)
+
+                if not serializer.is_valid():
+                    return Response({"state":0, "message":"Невалидный запрос"})
+                d = serializer.data
+
+                coords = pereval.coords_id
+                coords.latitude = float(d['coords']['latitude'])
+                coords.longitude = float(d['coords']['longitude'])
+                coords.height = int(d['coords']['height'])
+
+                level = d['level']
+                pereval.spring = level['spring']
+                pereval.summer = level['summer']
+                pereval.autumn = level['autumn']
+                pereval.winter = level['winter']
+
+                old_per_imgs = PerevalImages.objects.filter(pereval_id=pereval)
+                old_imgs = []
+                for old_per_img in old_per_imgs:
+                    old_imgs.append(old_per_img.image_id)
+
+                for image in d.get('images'):
+                    img = Images(img=str.encode(image.get('data')),
+                                 title=image.get('title'))
+                    img.save()
+                    objects_.append(img)
+
+                    per_img = PerevalImages(pereval_id=pereval,
+                                            image_id=img)
+                    per_img.save()
+                    objects_.append(per_img)
+
+                pereval.beautyTitle = d['beauty_title']
+                pereval.title = d['title']
+                pereval.otherTitles = d['other_titles']
+                pereval.connect = d['connect']
+                pereval.add_time = d['add_time']
+                return pereval, coords, old_per_imgs, old_imgs
+            else:
+                return Response({"state":0, "message": "Запись не новая"})
+
+        except Exception as e:
+            for obj in objects_:
+                obj.delete()
+            return Response({"state": 0, "message": f"Ошибка {e}"})
+        finally:
+            try:
+                pereval.save()
+                coords.save()
+                for per_img in old_per_imgs:
+                    per_img.delete()
+                for old_img in old_imgs:
+                    old_img.delete()
+
+                return Response({"state": 1, "message":None})
+            except Exception:
+                for obj in objects_:
+                    obj.delete()
