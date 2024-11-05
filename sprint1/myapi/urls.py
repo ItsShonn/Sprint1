2abd6e6a9afd441bcdc62a2ce5983d5f7c359f3b
ItsShonn <leonardochoned@gmail.com>
2024-11-05 16:27:38 +0300

    did openapi yaml file

--- sprint1/myapi/urls.py
+++ sprint1/myapi/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from .views import submitDataPOST, submitDataGET, submitDataEMAILGET
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('submitData', submitDataPOST.as_view()),
     path('submitData/<int:pk>', submitDataGET.as_view()),
     path('submitData/', submitDataEMAILGET.as_view()),
+    path(
+        "openapi",
+        get_schema_view(
+            title="FSTRAPI", description="API for FSTR app", version="1.0.0", urlconf='myapi.urls'
+        ),
+        name="openapi-schema"),
 ]
